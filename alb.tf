de0a6eee717376616d9e6f108ebae547189748e3
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-18 16:00:02 -0400

    frontend

--- alb.tf
+++ alb.tf
@@ -3,6 +3,15 @@ resource "aws_lb_target_group" "tg" {
   port     = var.APP_PORT
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
+  health_check {
+    enabled = true
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    interval = 5
+    path = "/health"
+    port = var.APP_PORT
+    timeout = 3
+  }
 }
 
 resource "aws_lb_target_group_attachment" "tg-attach" {
