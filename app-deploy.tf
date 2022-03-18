30eeb3162e812a9d2026b6a2e90769ceabc83034
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-18 16:16:51 -0400

    frontend

--- app-deploy.tf
+++ app-deploy.tf
@@ -1,6 +1,7 @@
 resource "null_resource" "app-deploy" {
   triggers = {
     instance_ids = join(",", aws_spot_instance_request.ec2-spot.*.spot_instance_id)
+    app_version = var.APP_VERSION
   }
   count       = length(aws_spot_instance_request.ec2-spot)
   provisioner "remote-exec" {
